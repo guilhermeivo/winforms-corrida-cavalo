d0974dc8cc62a48d038769d729987121fa240b2d
Guilherme Ivo <guilhermeivob@gmail.com>
2020-11-15 17:14:24 -0300

    update cavalo

--- CorridaCavalo/model/Cavalo.cs
+++ CorridaCavalo/model/Cavalo.cs
@@ -8,5 +8,59 @@ namespace CorridaCavalo.model
 {
     class Cavalo
     {
+        /// <summary>Primary key</summary> 
+        private int idCavalo;
+        /// <summary>Foreign key</summary> 
+        private int idDono;
+        /// <summary>Foreign key</summary> 
+        private int idStatus;
+        private String nome;
+        private int idade;
+
+        // idCavalo Methods
+        public void setIdCavalo(int idCavalo)
+        {
+            this.idCavalo = idCavalo;
+        }
+        public int getIdCavalo()
+        {
+            return idCavalo;
+        }
+        // idDono Methods
+        public void setIdNono(int idDono)
+        {
+            this.idDono = idDono;
+        }
+        public int getIdNono()
+        {
+            return idDono;
+        }
+        // idStatus Methods
+        public void setIdStatus(int idStatus)
+        {
+            this.idStatus = idStatus;
+        }
+        public int getIdStatus()
+        {
+            return idStatus;
+        }
+        // nome Methods
+        public void setNome(String nome)
+        {
+            this.nome = nome;
+        }
+        public String getNome()
+        {
+            return nome;
+        }
+        // idade Methods
+        public void setIdade(int idade)
+        {
+            this.idade = idade;
+        }
+        public int getIdade()
+        {
+            return idade;
+        }
     }
 }
