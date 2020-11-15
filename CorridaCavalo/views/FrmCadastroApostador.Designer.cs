736f4c6d77a1e22482bb49a59e4778d3e4b8b27e
Pedro Costa <pedropcs608@gmail.com>
2020-11-15 15:13:21 -0300

    oioi

--- CorridaCavalo/views/FrmCadastroApostador.Designer.cs
+++ CorridaCavalo/views/FrmCadastroApostador.Designer.cs
@@ -29,6 +29,16 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblTel = new System.Windows.Forms.Label();
+            this.lblemail = new System.Windows.Forms.Label();
+            this.lblDindin = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtTel = new System.Windows.Forms.TextBox();
+            this.txtemail = new System.Windows.Forms.TextBox();
+            this.txtvalor = new System.Windows.Forms.TextBox();
+            this.btnCadatro = new System.Windows.Forms.Button();
+            this.btnConsulta = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -38,9 +48,96 @@
             this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(221, 25);
+            this.label1.Size = new System.Drawing.Size(217, 29);
             this.label1.TabIndex = 1;
             this.label1.Text = "Cadastro apostador";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.ForeColor = System.Drawing.Color.White;
+            this.lblNome.Location = new System.Drawing.Point(76, 108);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(35, 13);
+            this.lblNome.TabIndex = 2;
+            this.lblNome.Text = "Nome";
+            // 
+            // lblTel
+            // 
+            this.lblTel.AutoSize = true;
+            this.lblTel.ForeColor = System.Drawing.Color.White;
+            this.lblTel.Location = new System.Drawing.Point(80, 149);
+            this.lblTel.Name = "lblTel";
+            this.lblTel.Size = new System.Drawing.Size(49, 13);
+            this.lblTel.TabIndex = 3;
+            this.lblTel.Text = "Telefone";
+            // 
+            // lblemail
+            // 
+            this.lblemail.AutoSize = true;
+            this.lblemail.ForeColor = System.Drawing.Color.White;
+            this.lblemail.Location = new System.Drawing.Point(80, 196);
+            this.lblemail.Name = "lblemail";
+            this.lblemail.Size = new System.Drawing.Size(31, 13);
+            this.lblemail.TabIndex = 4;
+            this.lblemail.Text = "email";
+            // 
+            // lblDindin
+            // 
+            this.lblDindin.AutoSize = true;
+            this.lblDindin.ForeColor = System.Drawing.Color.White;
+            this.lblDindin.Location = new System.Drawing.Point(80, 236);
+            this.lblDindin.Name = "lblDindin";
+            this.lblDindin.Size = new System.Drawing.Size(77, 13);
+            this.lblDindin.TabIndex = 5;
+            this.lblDindin.Text = "Valor Restante";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(211, 105);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 6;
+            // 
+            // txtTel
+            // 
+            this.txtTel.Location = new System.Drawing.Point(211, 142);
+            this.txtTel.Name = "txtTel";
+            this.txtTel.Size = new System.Drawing.Size(100, 20);
+            this.txtTel.TabIndex = 7;
+            // 
+            // txtemail
+            // 
+            this.txtemail.Location = new System.Drawing.Point(211, 193);
+            this.txtemail.Name = "txtemail";
+            this.txtemail.Size = new System.Drawing.Size(100, 20);
+            this.txtemail.TabIndex = 8;
+            // 
+            // txtvalor
+            // 
+            this.txtvalor.Location = new System.Drawing.Point(211, 236);
+            this.txtvalor.Name = "txtvalor";
+            this.txtvalor.Size = new System.Drawing.Size(100, 20);
+            this.txtvalor.TabIndex = 9;
+            // 
+            // btnCadatro
+            // 
+            this.btnCadatro.Location = new System.Drawing.Point(79, 299);
+            this.btnCadatro.Name = "btnCadatro";
+            this.btnCadatro.Size = new System.Drawing.Size(75, 23);
+            this.btnCadatro.TabIndex = 10;
+            this.btnCadatro.Text = "Cadastrar";
+            this.btnCadatro.UseVisualStyleBackColor = true;
+            this.btnCadatro.Click += new System.EventHandler(this.btnCadatro_Click);
+            // 
+            // btnConsulta
+            // 
+            this.btnConsulta.Location = new System.Drawing.Point(273, 299);
+            this.btnConsulta.Name = "btnConsulta";
+            this.btnConsulta.Size = new System.Drawing.Size(75, 23);
+            this.btnConsulta.TabIndex = 11;
+            this.btnConsulta.Text = "Consultas";
+            this.btnConsulta.UseVisualStyleBackColor = true;
             // 
             // FrmCadastroApostador
             // 
@@ -48,6 +145,16 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
             this.ClientSize = new System.Drawing.Size(780, 657);
+            this.Controls.Add(this.btnConsulta);
+            this.Controls.Add(this.btnCadatro);
+            this.Controls.Add(this.txtvalor);
+            this.Controls.Add(this.txtemail);
+            this.Controls.Add(this.txtTel);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblDindin);
+            this.Controls.Add(this.lblemail);
+            this.Controls.Add(this.lblTel);
+            this.Controls.Add(this.lblNome);
             this.Controls.Add(this.label1);
             this.Name = "FrmCadastroApostador";
             this.Text = "FrmCadastroApostador";
@@ -59,5 +166,15 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblTel;
+        private System.Windows.Forms.Label lblemail;
+        private System.Windows.Forms.Label lblDindin;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtTel;
+        private System.Windows.Forms.TextBox txtemail;
+        private System.Windows.Forms.TextBox txtvalor;
+        private System.Windows.Forms.Button btnCadatro;
+        private System.Windows.Forms.Button btnConsulta;
     }
 }
--- CorridaCavalo/views/FrmCadastroCavalo.Designer.cs
+++ CorridaCavalo/views/FrmCadastroCavalo.Designer.cs
@@ -29,6 +29,14 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.lblNomeCav = new System.Windows.Forms.Label();
+            this.lblNomeDono = new System.Windows.Forms.Label();
+            this.lblIdade = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.txtDono = new System.Windows.Forms.TextBox();
+            this.txtIdade = new System.Windows.Forms.TextBox();
+            this.txtStatus = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -38,9 +46,77 @@
             this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(184, 25);
+            this.label1.Size = new System.Drawing.Size(181, 29);
             this.label1.TabIndex = 1;
             this.label1.Text = "Cadastro Cavalo";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(221, 109);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 2;
+            // 
+            // lblNomeCav
+            // 
+            this.lblNomeCav.AutoSize = true;
+            this.lblNomeCav.ForeColor = System.Drawing.Color.White;
+            this.lblNomeCav.Location = new System.Drawing.Point(47, 112);
+            this.lblNomeCav.Name = "lblNomeCav";
+            this.lblNomeCav.Size = new System.Drawing.Size(86, 13);
+            this.lblNomeCav.TabIndex = 3;
+            this.lblNomeCav.Text = "Nome do Cavalo";
+            // 
+            // lblNomeDono
+            // 
+            this.lblNomeDono.AutoSize = true;
+            this.lblNomeDono.ForeColor = System.Drawing.Color.White;
+            this.lblNomeDono.Location = new System.Drawing.Point(47, 162);
+            this.lblNomeDono.Name = "lblNomeDono";
+            this.lblNomeDono.Size = new System.Drawing.Size(79, 13);
+            this.lblNomeDono.TabIndex = 4;
+            this.lblNomeDono.Text = "Nome do Dono";
+            // 
+            // lblIdade
+            // 
+            this.lblIdade.AutoSize = true;
+            this.lblIdade.ForeColor = System.Drawing.Color.White;
+            this.lblIdade.Location = new System.Drawing.Point(48, 211);
+            this.lblIdade.Name = "lblIdade";
+            this.lblIdade.Size = new System.Drawing.Size(85, 13);
+            this.lblIdade.TabIndex = 5;
+            this.lblIdade.Text = "Idade do Cavalo";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.ForeColor = System.Drawing.Color.White;
+            this.lblStatus.Location = new System.Drawing.Point(48, 254);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(37, 13);
+            this.lblStatus.TabIndex = 6;
+            this.lblStatus.Text = "Status";
+            // 
+            // txtDono
+            // 
+            this.txtDono.Location = new System.Drawing.Point(221, 162);
+            this.txtDono.Name = "txtDono";
+            this.txtDono.Size = new System.Drawing.Size(100, 20);
+            this.txtDono.TabIndex = 7;
+            // 
+            // txtIdade
+            // 
+            this.txtIdade.Location = new System.Drawing.Point(221, 211);
+            this.txtIdade.Name = "txtIdade";
+            this.txtIdade.Size = new System.Drawing.Size(100, 20);
+            this.txtIdade.TabIndex = 8;
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Location = new System.Drawing.Point(221, 254);
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(100, 20);
+            this.txtStatus.TabIndex = 9;
             // 
             // FrmCadastroCavalo
             // 
@@ -48,6 +124,14 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
             this.ClientSize = new System.Drawing.Size(780, 657);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.txtIdade);
+            this.Controls.Add(this.txtDono);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblIdade);
+            this.Controls.Add(this.lblNomeDono);
+            this.Controls.Add(this.lblNomeCav);
+            this.Controls.Add(this.txtNome);
             this.Controls.Add(this.label1);
             this.Name = "FrmCadastroCavalo";
             this.Text = "FrmCadastroCavalo";
@@ -59,5 +143,13 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label lblNomeCav;
+        private System.Windows.Forms.Label lblNomeDono;
+        private System.Windows.Forms.Label lblIdade;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.TextBox txtDono;
+        private System.Windows.Forms.TextBox txtIdade;
+        private System.Windows.Forms.TextBox txtStatus;
     }
 }
